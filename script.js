7d50e7b91283db3ba9fef0835a455c2ef69e4d56
Chuck Konkol <ckonkol@gmail.com>
2017-04-17 20:41:15 -0500

    vk

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function passlist()
    var api_key = "R_f2f3c9387a374e3fc6bf4b1ec2c945c4";
    var long_url = "https://rvclist.github.io/index.html?list="+ shoppinglist;
   try{
-  jQuery.getJSON(
+  $.getJSON(
              "http://api.bitly.com/v3/shorten?callback=?",
               {
              "format": "json",

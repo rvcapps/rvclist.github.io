4e060e969ae2f41d84c197d659d52a9ed2d8715c
Chuck Konkol <ckonkol@gmail.com>
2017-04-17 20:05:42 -0500

    ck

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ function get(name){
     if(num<0)  return url;
 }
 //v4.1
-
-var getshorturl=0;
 function passlist()
 {
+   var getshorturl=0;
    var login = "o_3iokgmm945";
    var api_key = "R_f2f3c9387a374e3fc6bf4b1ec2c945c4";
    var long_url = "https://rvclist.github.io/index.html?list="+ shoppinglist;
-   $.getJSON(
+  try{
+  $.getJSON(
              "http://api.bitly.com/v3/shorten?callback=?",
               {
              "format": "json",
@@ -36,20 +36,16 @@ function passlist()
                 getshorturl = 1;
                 document.getElementById("sharelist").innerHTML = 'Share URL:\n' + response.data.url;
                 copyToClipboard(response.data.url);
-             }
-              );
+             });
+  } catch(err) {
+   alert("wow");
+   alert("Error : "+ err);
+}
 }
 //v4.1
 function share()
 {
    passlist();
-      var long_url = "https://rvclist.github.io/index.html?list="+ shoppinglist;
-    alert(getshorturl);
-    if (getshorturl==0)
-    {
-       document.getElementById("sharelist").innerHTML = 'Share URL:\n' + long_url;
-       copyToClipboard(long_url);
-    }
 }
 
 function copyToClipboard(text) {

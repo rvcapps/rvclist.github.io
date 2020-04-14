3c8ef7db1b8b091d5c77698a0076e4ff4dc72570
Chuck Konkol <ckonkol@gmail.com>
2020-04-14 05:14:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//v3.4 Add popup describing app when visitors load webpage the first time
+window.onload = function() {
+    alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+    populateshoppinglistonload();
+    displayShoppinglists();
+    clearFocus();
+};
+
 //v 4.0 save / get array via cookies
 //v 4.0 read cookie on load and display
 
@@ -55,13 +63,6 @@ function share()
 function copyToClipboard(text) {
    window.prompt("Copy & Share List!", text);
 }
-//v4.0 Add popup describing app when visitors load webpage the first time
-window.onload = function() {
-    alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
-    populateshoppinglistonload();
-    displayShoppinglists();
-    clearFocus();
-};
 
 function about()
 {
